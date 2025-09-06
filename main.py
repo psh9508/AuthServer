961e709548e51c8672c8baef2ca18baddf5bf72a
masuri <psh0258@gmail.com>
2025-09-06 10:33:10 +0900

    fast api smoke test

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from authserver!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def fire():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
