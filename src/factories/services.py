b41bd4aca134925333d56e8b60fe333c6950ef13
masuri <psh0258@gmail.com>
2025-09-23 22:19:35 +0900

    feat: add __init__ file to the services folder

--- src/factories/services.py
+++ src/factories/services.py
@@ -5,8 +5,7 @@ from src.factories.rebbitmq import get_message_queue_service
 from src.core.database import get_session
 from src.repositories.user_repository import UserRepository
 from src.services.message_queue_service import MessageQueueService, get_rabbitmq_client
-from src.services.user_service import UserService
-from src.services.auth_service import AuthService
+from src.services import UserService, AuthService
 
 def get_user_service(
       session: AsyncSession = Depends(get_session),
---
+++ src/services/__init__.py
@@ -0,0 +1,2 @@
+from .user_service import UserService
+from .auth_service import AuthService
