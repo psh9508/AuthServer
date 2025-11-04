534b75b0d3b0be9d612212c62b78bbbf09aabc50
masuri <psh0258@gmail.com>
2025-11-04 22:58:08 +0900

    refactor: streamline SSL connection handling in database initialization

--- src/core/database.py
+++ src/core/database.py
@@ -2,6 +2,8 @@ from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, AsyncEngine
 from config.config import get_config
 from typing import Optional
+import ssl
+import os
 
 async_engine: Optional[AsyncEngine] = None
 async_session: Optional[async_sessionmaker[AsyncSession]] = None
@@ -17,18 +19,21 @@ def init_db_session():
         f"@{db_config['host']}:{db_config.get('port', 5432)}/{db_config['database']}"
     )
     
-    import ssl
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = True
-    ssl_context.verify_mode = ssl.CERT_REQUIRED
-    ssl_context.load_verify_locations(db_config['ssl_path'])
+    connect_args = {}
+    
+    if db_config['host'] in ['localhost', '127.0.0.1']:
+        connect_args["ssl"] = False
+    else:
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = True
+        ssl_context.verify_mode = ssl.CERT_REQUIRED
+        ssl_context.load_verify_locations(db_config['ssl_path'])
+        connect_args["ssl"] = ssl_context
     
     async_engine = create_async_engine(
         DATABASE_URL,
         echo=True,
-        connect_args={
-            "ssl": ssl_context
-        }
+        connect_args=connect_args
     )
     
     async_session = async_sessionmaker(
