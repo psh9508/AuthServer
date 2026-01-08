9a8a08e1b48089116e03d88189fc5fc6aa95acd1
masuri <psh0258@gmail.com>
2026-01-08 15:35:45 +0900

    feat: retrieve login attempt count from Redis in alogin method

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -18,12 +18,16 @@ class UserService:
 
     async def alogin(self, login_id: str, password: str) -> LoginRes:
         # Before starting, get the login attempts for this email from Redis.
+        attempt_count = await self.redis_service.aget_login_attempts(login_id)
+        
         # If login fails, increase and save the attempts, and return the count too.
+
         # For login failure, handle it in try/except, and re-raise inside except.
 
         user = await self.user_repo.alogin(login_id, password)
         
         if not user:
+            await self.redis_service.aadd_login_attempts_count(login_id, attempt_count + 1, ttl=15*60)
             raise UserNotFoundError("Invalid credentials")
         elif not bool(user.email_verified):
             raise EmailNotVerifiedError(user_id= str(user.user_id))
