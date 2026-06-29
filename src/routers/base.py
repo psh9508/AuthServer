eace74e37a17b181e440cd45ae3421905b195fae
masuri <psh0258@gmail.com>
2026-06-29 10:24:57 +0900

    refactor: remove healthcheck endpoint and related imports from base router

--- src/routers/base.py
+++ src/routers/base.py
@@ -1,9 +1,3 @@
 from fastapi import APIRouter
-from src.routers.models.base import HealthCheckRes
-from src.routers.models.base_response_model import BaseResponseModel
 
 router = APIRouter()
-
-@router.get('/healthcheck', tags=['health'])
-async def healthcheck() -> BaseResponseModel:
-    return BaseResponseModel(data=HealthCheckRes(message="Service is healthy"))
